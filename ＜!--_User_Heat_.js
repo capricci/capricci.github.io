2abb01f18191e0fdabf215fd543fa8dca79ae12b
capricci <material155@yahoo.co.jp>
2016-03-20 21:44:46 +0900

    Rename ＜!--_User_Heat_.txt to ＜!--_User_Heat_.js

--- ＜!--_User_Heat_.js
+++ ＜!--_User_Heat_.js
@@ -21,5 +21,5 @@ function randobet(n){var a='123456789'.split(''),s='';for(var i=0;i<n;i++) s+=a[
 //-->
 </script>
 <noscript>
-<a href='http://smartphone.userlocal.jp/' target='_blank'><img src='http://le.nakanohito.jp/le/1/?id=6018512&h=6d49&lt=3&guid=ON&eflg=1' alt='ƒXƒ}[ƒgƒtƒHƒ“‰ðÍ' height='1' width='1' border='0' /></a>
+<a href='http://smartphone.userlocal.jp/' target='_blank'><img src='http://le.nakanohito.jp/le/1/?id=6018512&h=6d49&lt=3&guid=ON&eflg=1' alt='Æ’XÆ’}Â[Æ’gÆ’tÆ’HÆ’â€œâ€°Ã°ÂÃ' height='1' width='1' border='0' /></a>
 </noscript>
