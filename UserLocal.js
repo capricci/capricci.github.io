b5b7ec84ff5860efd0e7ab1b8a41e063570abc6d
capricci <material155@yahoo.co.jp>
2017-03-06 11:56:13 +0900

    Update UserLocal.js

--- UserLocal.js
+++ UserLocal.js
@@ -16,7 +16,3 @@ function getuid(key){var arr=[],date=new Date(),exp=new Date();exp.setFullYear(e
 function randobet(n){var a='123456789'.split(''),s='';for(var i=0;i<n;i++) s+=a[Math.floor(Math.random() * a.length)];return s;};
 })();
 //-->
-</script>
-<noscript>
-<a href='http://smartphone.userlocal.jp/' target='_blank'><img src='http://le.nakanohito.jp/le/1/?id=6018512&h=6d49&lt=3&guid=ON&eflg=1' alt='ƒXƒ}[ƒgƒtƒHƒ“‰ðÍ' height='1' width='1' border='0' /></a>
-</noscript>
